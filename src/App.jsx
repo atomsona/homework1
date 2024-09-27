d54fef9f186e14728cea3236546f724a1c80b874
gio <giozedelashvili@gmail.com>
2024-09-27 19:24:03 +0400

    test

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    function App() {
+      return (
+        <div className="App">
+          <h1 className="title">First Name: gio</h1>
+          <h1 className="title">Last Name: zedelashvili</h1>
+          <h1 className="title">Age: 14</h1>
+          <h1 className="title">Hobby: coding and basketball</h1>
+        </div>
+      );
+    }
+    
+    
+  )
+}
+
+export default App
